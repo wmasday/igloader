67e6f8434f5736dd7dcc9325064a85e13f1472a9
akuhidayat <hiidayt18@gmail.com>
2021-07-01 04:01:39 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
